0b8bb8b769a97a7b41414c8473367a1ea16eadc5
banbanchs <memory.silentvoyage@gmail.com>
2015-05-23 17:35:53 +0800

    Git init

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var ghPages = require('gulp-gh-pages');
+
+
+gulp.task('deploy', function() {
+  return gulp.src('./*').pipe(ghPages());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./*.js', ['js']);
+});
+
+gulp.task('serve', function() {
+  connect.server({
+    root: ['.'],
+    port: 5000,
+    livereload: true,
+    fallback: 'index.html'
+  });
+});
+
+
+gulp.task('default', ['watch', 'serve']);
